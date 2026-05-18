2643a28829fe48680b69ab9c1f8b14a150e44fb7
Joey Vagedes <joey.vagedes@gmail.com>
2026-05-18 21:55:51 +0000

    CryptoPkg: BaseCryptLib: Add GetAuthenticodeHashAlgorithm API

--- CryptoPkg/Test/Mock/Include/GoogleTest/Library/MockBaseCryptLib.h
+++ CryptoPkg/Test/Mock/Include/GoogleTest/Library/MockBaseCryptLib.h
@@ -953,6 +953,16 @@ struct MockBaseCryptLib {
     );
 
   MOCK_FUNCTION_DECLARATION (
+    EFI_STATUS,
+    GetAuthenticodeHashAlgorithm,
+    (
+     IN  CONST UINT8  *AuthData,
+     IN  UINTN        AuthDataSize,
+     OUT EFI_GUID     *HashType
+    )
+    );
+
+  MOCK_FUNCTION_DECLARATION (
     BOOLEAN,
     ImageTimestampVerify,
     (
